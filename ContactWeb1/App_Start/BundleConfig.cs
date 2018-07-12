8d6c58f4762472711932b4c68a70dcf5882d71d6
RoKiDev <romiki44@hotmail.com>
2018-07-12 19:27:49 +0200

    Initial commit

---
+++ ContactWeb1/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ContactWeb1
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/js/lib/bootstrap.min.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/dataTables.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/css/bootstrap.min.css",
+                      "~/css/datatables.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
---
+++ ContactWeb1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactWeb1.Startup))]
+namespace ContactWeb1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
